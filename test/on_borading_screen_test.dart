b1d3075794c4e6332a5e21ee3b233cefb5494dde
Raj <8091031-baliza-tech@users.noreply.gitlab.com>
2021-06-11 13:34:33 +0530

    _

--- test/on_borading_screen_test.dart
+++ test/on_borading_screen_test.dart
@@ -1,12 +1,12 @@
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:on_borading_screen/on_borading_screen.dart';
+// import 'package:on_borading_screen/on_borading_screen.dart';
 
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
-}
+// void main() {
+//   test('adds one to input values', () {
+//     final calculator = Calculator();
+//     expect(calculator.addOne(2), 3);
+//     expect(calculator.addOne(-7), -6);
+//     expect(calculator.addOne(0), 1);
+//   });
+// }
